cc6bc7086e45375a4eff467c2a91d936c91b927f
saqib <saqibkhatai@gmail.com>
2025-08-11 05:09:16 -0700

    project created and libreries installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import { useEffect } from "react";
-import "flowbite";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    import("flowbite");
-  }, []);
-
   return <div></div>;
 }
 
